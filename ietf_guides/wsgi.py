e09d419902c6fe395d97a5fac098647a7a0ff69a
Charlie Root <root@gitalo.rjsparks.org>
2019-02-21 13:03:47 -0600

    cleaned up settings a little. Added a real wsgi.py in prep for deployment.

--- ietf_guides/wsgi.py
+++ ietf_guides/wsgi.py
@@ -8,9 +8,21 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+from django.conf import settings
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ietf_guides.settings")
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Virtualenv support
+virtualenv_activation = os.path.join(path, "env", "bin", "activate_this.py")
+if os.path.exists(virtualenv_activation):
+    execfile(virtualenv_activation, dict(__file__=virtualenv_activation))
+
+if not path in sys.path:
+    sys.path.insert(0, path)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings.DJANGO_SETTINGS_MODULE or "ietf_guides.settings.prod")
 
 application = get_wsgi_application()

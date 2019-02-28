813caae1c6df09b76cd1d0a6a244c0aeda410760
Robert Sparks <rjsparks@nostrum.com>
2019-02-28 16:27:36 -0600

    Adding Travis

--- ietf_guides/settings/test.py
+++ ietf_guides/settings/test.py
@@ -1,3 +1,10 @@
 from .base import *
 
 EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}

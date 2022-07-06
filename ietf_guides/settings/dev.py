ce1aba7475892c0b7d0aedb650a01f3390c4a73e
removedporn <86824510+removedporn@users.noreply.github.com>
2022-07-06 16:24:24 +0800

    Delete ietf_guides directory

--- ietf_guides/settings/dev.py
+++
@@ -1,4 +0,0 @@
-from .base import *
-
-DEBUG = True
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- ietf_guides/settings/prod.py
+++
@@ -1,8 +0,0 @@
-from .base import *
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-# Your local.py should contain the credentials needed to send email through your selected SMTP
-# server. See <https://docs.djangoproject.com/en/2.1/topics/email/#obtaining-an-instance-of-an-email-backend>
-
-
